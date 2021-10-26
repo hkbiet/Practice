4f0966b22744b750b5eb152adc22a78163ab8448
HEMANT KUMAR <hkbiet@gmail.com>
2021-10-26 19:27:52 +0530

    Create interviewcake-inflight-entertainment.py

---
+++ interviewcake-inflight-entertainment.py
@@ -0,0 +1,78 @@
+import unittest
+
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+
+    # Determine if two movie runtimes add up to the flight length
+    movie_lengths.sort()
+    for i in range(len(movie_lengths)):
+        j = i + 1
+        if j <= len(movie_lengths)-1:
+            current_sum = movie_lengths[i] + movie_lengths[j]
+            if current_sum == flight_length:
+                return True
+        else:
+            pass
+
+
+    return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_multiple_movies_shorter_than_flight(self):
+        result = can_two_movies_fill_flight([5, 6, 7, 8], 9)
+        self.assertFalse(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
