38455f5809067c8178f8e3bf6708a493f6c13cd6
HEMANT KUMAR <hkbiet@gmail.com>
2023-11-23 10:31:14 +0530

    Create validateSubSequence-AllTestsPass.py
    AlgPt

---
+++ validateSubSequence-AllTestsPass.py
@@ -0,0 +1,16 @@
+def isValidSubsequence(array, sequence):
+    sequence_traverser = 0
+    array_traverser = 0
+
+    while sequence_traverser < len(sequence) and array_traverser < len(array):
+        if array[array_traverser] == sequence[sequence_traverser]:
+            array_traverser = array_traverser + 1
+            sequence_traverser = sequence_traverser + 1
+        else:
+            array_traverser = array_traverser + 1
+    if sequence_traverser == len(sequence):
+        if sequence[sequence_traverser-1] == array[array_traverser-1]:
+            return True
+        else:
+            return False
+    return False
