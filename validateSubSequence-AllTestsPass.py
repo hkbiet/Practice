65150a3ecfdf7ce1cae94d66a0196f83e69ac666
HEMANT KUMAR <hkbiet@gmail.com>
2023-11-23 10:48:09 +0530

    removed redundant increment | AlgPt

--- validateSubSequence-AllTestsPass.py
+++ validateSubSequence-AllTestsPass.py
@@ -5,10 +5,8 @@ def isValidSubsequence(array, sequence):
 
     while sequence_traverser < len(sequence) and array_traverser < len(array):
         if array[array_traverser] == sequence[sequence_traverser]:
-            array_traverser = array_traverser + 1
             sequence_traverser = sequence_traverser + 1
-        else:
-            array_traverser = array_traverser + 1
+        array_traverser = array_traverser + 1
     if sequence_traverser == len(sequence):
         if sequence[sequence_traverser-1] == array[array_traverser-1]:
             return True
